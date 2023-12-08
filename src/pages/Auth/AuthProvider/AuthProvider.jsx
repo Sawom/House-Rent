988efbde9bb67c71415cf30d652ad9c99e8823e5
Sawom <asawom250@gmail.com>
2023-12-08 23:28:15 +0600

    auth updated

--- src/pages/Auth/AuthProvider/AuthProvider.jsx
+++ src/pages/Auth/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createContext } from 'react';
+import useFirebase from '../useFirebase/useFirebase'
 
 export const AuthContext = createContext({}); 
 
 const AuthProvider = ( { children } ) => {
+    const allContexts = useFirebase();
     return (
         <AuthContext.Provider value={allContexts}>
             {children}
