55a1c02a82e6e18c66a2d825bddb2296e54c9ce5
Sawom <asawom250@gmail.com>
2023-12-08 22:28:35 +0600

    tanstack firebase added

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,36 @@ import Home from "./pages/Home/Home";
 import PropertyInfo from "./pages/PropertyInfo/PropertyInfo";
 import SavedProperty from "./components/SavedProperty/SavedProperty";
 
+// tanstack query added
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import AuthProvider from "./pages/Auth/AuthProvider/AuthProvider";
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<NavLayout />}>
-          <Route index exact element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="add-property" element={<AddProperty />} />
-        </Route>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/property-info" element={<PropertyInfo />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/saved-property" element={<SavedProperty />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+         <QueryClientProvider client={queryClient} >
+            <Router>
+              <Routes>
+                  <Route path="/" element={<NavLayout />}>
+                    <Route index exact element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="signup" element={<Signup />} />
+                    <Route path="add-property" element={<AddProperty />} />
+                  </Route>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/property-info" element={<PropertyInfo />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/saved-property" element={<SavedProperty />} />
+              </Routes>
+          </Router>
+         </QueryClientProvider>
+    </AuthProvider>
+
+    
   );
 }
 
