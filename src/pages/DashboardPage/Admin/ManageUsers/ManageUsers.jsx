b8166a35be00a54cd2c9a1edc036b59839020387
Sawom <asawom250@gmail.com>
2023-12-12 17:34:33 +0600

    admin done

--- src/pages/DashboardPage/Admin/ManageUsers/ManageUsers.jsx
+++ src/pages/DashboardPage/Admin/ManageUsers/ManageUsers.jsx
@@ -8,14 +8,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
+import Person3Icon from '@mui/icons-material/Person3';
 import Swal from 'sweetalert2';
 
 
 const ManageUsers = () => {
-    const {data: rent = [], isLoading: loading, refetch } = useQuery({
-        queryKey: ['rent'],
+    const {data: users = [], isLoading: loading, refetch } = useQuery({
+        queryKey: ['users'],
         queryFn: async() =>{
-            const res = await fetch('http://localhost:5000/rent');
+            const res = await fetch('http://localhost:5000/users');
             return res.json()
         }
     })
@@ -27,14 +28,23 @@ const ManageUsers = () => {
     // })
 
     // make admin function
-    const handleMakeAdmin = user =>{
-        fetch(`http://localhost:5000/users/admin/${user._id}`, {
-            method: 'PATCH'
+    const handleMakeAdmin = (user) =>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Make Admin!'
+        }).then( (result)=>{
+            if(result.isConfirmed){
+                fetch(`http://localhost:5000/users/admin/${user._id}`, {
+                method: 'PATCH'
         })
         .then(res => res.json())
-        .then(data =>{
-            console.log(data)
-            if(data.modifiedCount){
+        .then( (data) =>{
+            if(data.modifiedCount ){
                 refetch();
                 Swal.fire({
                     position: 'top-end',
@@ -43,8 +53,11 @@ const ManageUsers = () => {
                     showConfirmButton: false,
                     timer: 1500
                   })
+                
+              }
+                } )
             }
-        })
+        } )
     }
 
      // user delete function 
@@ -86,21 +99,39 @@ const ManageUsers = () => {
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                 <TableRow>
+                    <TableCell>#</TableCell>
                     <TableCell>Name </TableCell>
                     <TableCell align="right">Email</TableCell>
                     <TableCell align="right">Phone</TableCell>
                     <TableCell align="right">Account Type</TableCell>
-                    <TableCell align="right">Protein(g)</TableCell>
                     <TableCell align="right">Role</TableCell>
                     <TableCell align="right">Action</TableCell>
                 </TableRow>
                 </TableHead>
                 <TableBody>
                 
+                {
+                    users.map( (user, index) =>  <TableRow key={user._id}  >
+                        <TableCell align="right"> {index + 1} </TableCell>
+                        <TableCell align="right">{user.name}</TableCell>
+                        <TableCell align="right">{user.email}</TableCell>
+                        <TableCell align="right">{user.phone}</TableCell>
+                        <TableCell align="right">{user.type}</TableCell>
+                        {/* user role */}
+                        <TableCell align="right">
+                            {
+                                user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className=" bg-blue-700
+                                    "> <Person3Icon></Person3Icon> </button>
+                            }
+                             
+                        </TableCell>
+                        <TableCell align="right"></TableCell>
+                        
+                    </TableRow>
+                     )
+                }
 
-                <TableRow>
-                    <TableCell align="right"></TableCell>
-                </TableRow>
+                
 
 
                 </TableBody>
